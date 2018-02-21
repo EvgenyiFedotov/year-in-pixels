cbc933f8aebd47b96ff61b42bc5c33d972a07b65
EvgenyiFedotov <fedotov_e1290@mail.ru>
2018-02-21 23:38:46 +0300

    Билдер

--- www/Service/Responses/Success/Min.php
+++ www/Service/Responses/Success/Min.php
@@ -5,197 +5,12 @@
     * Минимизация файлов
     */
    Query\route([
-      "route" => "min",
+      "route" => "builder",
       "type" => "success",
       "priority" => 900,
       "handler" => function($require, $route, $configRoute, $query) {
-         /**
-          * Проверить является ли путь исключением
-          * @param {String} $path
-          * @param {Array.<String>} [$exceptions]
-          */
-         function isExceptionPath($path, $exceptions = []) {
-            $checkPath = explode("/", $path);
-            array_shift($checkPath);
-            $checkPath = join("/", $checkPath);
-            $isException = false;
-            $matchResult = [];
-
-            foreach ($exceptions as $indexExc => $exception) {
-               preg_match_all("/^". $exception . "/", $checkPath, $matchResult);
-
-               if (count($matchResult[0])) {
-                  $isException = true;
-               }
-            }
-
-            return $isException;
-         };
-
-         /**
-          * Преобразовать путь до пути минимальной версии
-          * @param {String} $root
-          * @param {String} $path
-          */
-         function toPathMin($root, $path) {
-            $pathArr = explode("/", $path);
-            $pathArr[0] = $root;
-            return join("/", $pathArr);
-         };
-
-         /**
-          * Копировать файл
-          * @param {String} $pathFrom
-          * @param {String} $pathIn
-          */
-         function copyFile($pathFrom, $pathIn) {
-            $content = file_get_contents($pathFrom);
-
-            // Минимазация контента
-            $pathInfo = pathinfo($pathFrom);
-            $extension = $pathInfo["extension"];
-
-            if ($extension === "js") {
-               $minifier = new Minify\JS();
-               $minifier->add($content);
-               $content = $minifier->minify();
-            } elseif ($extension === "css") {
-               $minifier = new Minify\CSS();
-               $minifier->add($content);
-               $content = $minifier->minify();
-            }
-
-            file_put_contents($pathIn, $content);
-         };
-
-         /**
-          * Копировать дирректорию
-          * 
-          * @param {String} $pathFrom
-          * @param {String} $pathIn
-          * @param {Array.<String>} [$exceptions] (Передаются как регулярные выражения)
-          * @param {Array.<String>} [$includes] (Передаются как регулярные выражения)
-          */
-         function cloneFolder($require, $pathFrom, $pathIn, $exceptions = [], $includes = []) {
-            // Пути до файлов и дирректорий
-            $paths = $require->pathFilesDir($pathFrom, true);
-
-            // Создадим корневую папку минимизированных файлов
-            if (!file_exists($pathIn)) {
-               mkdir($pathIn);
-            }
-
-            echo "<b>Clone:</b></br>";
-            echo "<b><i>Folders:</i></b></br>";
-
-            // Склонируем папки в минимальную версию
-            foreach ($paths["dirs"] as $key => $path) {
-               // Узнаем иссключение или нет
-               $isException = isExceptionPath($path, $exceptions);
-
-               // Если не является исключением, копируем
-               if ($isException === false) {
-                  $pathMin = toPathMin($pathIn, $path);
-                  echo $pathMin . "</br>";
-
-                  if (!file_exists($pathMin)) {
-                     mkdir($pathMin);
-                  }
-               }
-            }
-
-            echo "</br><b><i>Folders includes:</i></b></br>";
-
-            // Скопируем папки (Включения)
-            foreach ($includes as $index => $path) {
-               $pathArr = explode("/", $path);
-               $pathCheckDir = $pathFrom;
-               $pathMin = $pathIn;
-
-               foreach ($pathArr as $indexPathEl => $pathEl) {
-                  $pathMin .= "/" . $pathEl;
-                  $pathCheckDir .= "/" . $pathEl;
-
-                  if (is_dir($pathCheckDir)) {
-                     echo $pathMin . "</br>";
-
-                     if (!file_exists($pathMin)) {
-                        mkdir($pathMin);
-                     }
-                  }
-               }
-            }
-
-            echo "</br>";
-            echo "<b><i>Files:</i></b></br>";
-
-            // Скопируем файлы в минимальную версию
-            foreach ($paths["files"] as $index => $path) {
-               // Узнаем иссключение или нет
-               $isException = isExceptionPath($path, $exceptions);
-
-               // Если не является исключением, копируем
-               if ($isException === false) {
-                  $pathMin = toPathMin($pathIn, $path);
-
-                  copyFile($path, $pathMin);
-
-                  echo $pathMin . "</br>";
-               }
-            }
-
-            echo "</br><b><i>Files includes:</i></b></br>";
-
-            // Скопируем файлы в минимальную версию (Включения)
-            foreach ($includes as $index => $path) {
-               /**
-                * Если передали путь до дирректории, значит копируем все файлы внутри,
-                * но не рекурсивно
-                */
-               if (is_dir($pathFrom . "/" . $path)) {
-                  $files = $require->pathFilesDir($pathFrom . "/" . $path);
-                  $files = $files["files"];
-
-                  foreach ($files as $indFile => $pathFile) {
-                     $pathMin = toPathMin($pathIn, $pathFile);
-
-                     copyFile($pathFile, $pathMin);
-
-                     echo $pathMin . "</br>";
-                  }
-               } else {
-                  $path = $pathFrom . "/" . $path;
-                  $pathMin = toPathMin($pathIn, $path);
-
-                  copyFile($path, $pathMin);
-
-                  echo $pathMin . "</br>";
-               }
-            }
-         };
-
-         /**
-          * Удалить дирректорию
-          * @param {String} $path
-          */
-         function removeFolder($require, $path) {
-            $paths = $require->pathFilesDir($path);
-
-            // Удалим папки
-            foreach ($paths["dirs"] as $index => $pathDir) {
-               removeFolder($require, $pathDir);
-            }
-
-            // Удалим файлы
-            foreach ($paths["files"] as $index => $pathFile) {
-               unlink($pathFile);
-            }
-
-            rmdir($path);
-         };
-
          // Подключим файлы необходимые для минимзации файлов
-         $require->includeFiles(["min"]);
+         $require->includeFiles(["min", "Core/Builder.php"]);
 
          // Корень директории из которой переносим файлы
          $pathFrom = "Client";
@@ -217,13 +32,45 @@
             "Librarys/Backbone/backbone.js"
          ];
 
+         /**
+          * Обработчик контента
+          * @param {Array.<String>} $pathInfo
+          * @param {String} $content
+          */
+         $handlerContent = function($pathInfo, $content) {
+            $extension = $pathInfo["extension"];
+
+            // Минимизация контента
+            if ($extension === "js") {
+               $minifier = new Minify\JS();
+               $minifier->add($content);
+               $content = $minifier->minify();
+            } elseif ($extension === "css") {
+               $minifier = new Minify\CSS();
+               $minifier->add($content);
+               $content = $minifier->minify();
+            }
+
+            return $content;
+         };
+
          $data = $query->data();
          $action = $data["action"];
 
+         $builder = new Builder([
+            "pathFrom" => "Client",
+            "pathIn" => "Client/min",
+            "exceptions" => $exceptions,
+            "includes" => $includes,
+            "handlerContent" => $handlerContent
+         ]);
+
          if ($action === "remove") {
-            removeFolder($require, $pathIn);
+            $builder->removeFolder($pathIn);
          } elseif ($action === "clone") {
-            cloneFolder($require, $pathFrom, $pathIn, $exceptions, $includes);
+            $builder->cloneFolder($pathFrom, $pathIn, $exceptions, $includes, $handlerContent);
+         } elseif ($action === "build") {
+            $builder->build();
          }
 
          exit;
